77023eca7c60c9c93ca8d3273a5aa6a3db5b52eb
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-18 18:22:15 -0400

    fix: #attr_reader name

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,9 @@
 class Ship
 
-  attr_reader :ship_name, :length, :health
+  attr_reader :name, :length, :health
 
   def initialize(ship_name, length)
-    @ship_name = ship_name
+    @name = ship_name
     @length = length
     @health = @length
   end
